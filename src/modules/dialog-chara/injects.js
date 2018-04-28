7d4b82b29326a03ea1e37ed1468591c327fd7320
Jet Rico <jetriconew@gmail.com>
2018-04-29 04:33:08 +0800

    Module: Dialog-Character

---
+++ src/modules/dialog-chara/injects.js
@@ -0,0 +1,43 @@
+import HioriSDK from '@sdk'
+
+class DialogChara extends HioriSDK.ContentScript {
+
+  constructor() {
+    super()
+    this.lang = 'en'
+    this.lastScene = null
+  }
+
+  run() {
+    console.info('! dialog-chara !')
+    setInterval(this.checkSceneChange.bind(this), 2000)
+  }
+
+  checkSceneChange() {
+    // If last scene is same as current scene, no change
+    if (!window.aoba) return
+    if (this.lastScene === window.aoba.sceneManager.currentScene) return
+
+    // Assign current scene as the last
+    this.lastScene = window.aoba.sceneManager.currentScene
+
+    // Scene has changed
+    this.sceneChanged()
+  }
+
+  sceneChanged() {
+    console.log('Scene changed')
+    // Check if scene has dialog
+    if (!aoba) return
+    if (!aoba.sceneManager) return
+    if (!aoba.sceneManager.currentScene) return
+    if (!aoba.sceneManager.currentScene.children.length) return
+    if (!aoba.sceneManager.currentScene.children[0].children.length) return
+    if (!aoba.sceneManager.currentScene.children[0].children[0]._eventTracks) return
+    // Confirmed it has dialog
+    console.log('DIALOG', aoba.sceneManager.currentScene.children[0].children[0]._eventTracks)
+  }
+
+}
+
+export default DialogChara
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,7 +1,11 @@
 import ReplacerBackground from './replacer/background'
+import DialogCharaInject from './dialog-chara/injects'
 
 export default {
   Replacer: {
     background: ReplacerBackground,
+  },
+  DialogChara: {
+    injects: DialogCharaInject
   }
 }
