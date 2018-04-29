e356fb14b895b0704f67633078f77695d5d520f5
Jet Rico <jetriconew@gmail.com>
2018-04-29 16:19:47 +0800

    Part 1 update to the scene checks

--- src/modules/dialog/injects.js
+++ src/modules/dialog/injects.js
@@ -13,7 +13,7 @@ class Dialog extends HioriSDK.ContentScript {
   run() {
     console.info('! dialog !')
     this.loadTranslations()
-    setInterval(this.detectDialog.bind(this), 500)
+    setInterval(this.checkScene.bind(this), 100)
   }
 
   loadTranslations() {
@@ -25,40 +25,49 @@ class Dialog extends HioriSDK.ContentScript {
     })
   }
 
-  hasSceneChanged() {
-    // Check if scene has changed from last check
-    let sceneChanged = this.lastScene !== window.aoba.sceneManager.currentScene
-
-    // Assign current scene as the last
-    this.lastScene = window.aoba.sceneManager.currentScene
-
-    // Return result
-    return sceneChanged
-  }
-
-  detectDialog() {
-    // Check if scene has dialog
+  checkScene() {
+    // Check if the scene has changed
     if (!window.aoba) return
     if (!window.aoba.sceneManager) return
     if (!window.aoba.sceneManager.currentScene) return
-    if (!window.aoba.sceneManager.currentScene.children.length) return
-    if (!window.aoba.sceneManager.currentScene.children[0].children.length) return
-    if (!window.aoba.sceneManager.currentScene.children[0].children[0]._eventTracks) return
+    if (this.lastScene === window.aoba.sceneManager.currentScene) return
+    this.lastScene = window.aoba.sceneManager.currentScene
+    console.log('sceneChanged', this.lastScene)
+    // Register enter listener to wait for resources
+    setTimeout(() => {
+      console.log('sceneEntered', this.lastScene)
+      this.detectDialog(this.lastScene)
+    }, 100)
+  }
 
-    // Confirmed it has dialog, check if the scene has changed
-    if (this.hasSceneChanged()) {
-      // There is dialog, and this is a new scene, handle translations
-      let dialogList = window.aoba.sceneManager.currentScene.children[0].children[0]._eventTracks
-      dialogList = this.translate(dialogList)
-    }
+  detectDialog(scene) {
+    console.log('detecting dialog', scene);
+    // Check if scene has dialog
+    this.findDialogFromScene(scene)
+    scene.children.forEach(sceneChildrenL1 => {
+      this.findDialogFromScene(sceneChildrenL1)
+      sceneChildrenL1.children.forEach(sceneChildrenL2 => {
+        this.findDialogFromScene(sceneChildrenL2)
+      })
+    })
+  }
+
+  findDialogFromScene(scene) {
+    // Regular dialog
+    if (scene._eventTracks)
+      this.translate(scene._eventTracks)
+    // Event dialogs
+    if (scene._trackManager && scene._trackManager._tracks)
+      this.translate(scene._eventTracks)
   }
 
   translate(dialogList) {
+    console.log('translating', dialogList)
     // Translate the full dialog event
     return dialogList.map(dialog => {
-      if (this.translations[dialog.speaker])
+      if (dialog.speaker && this.translations[dialog.speaker])
         dialog.speaker = this.translations[dialog.speaker]
-      if (this.translations[dialog.text])
+      if (dialog.text && this.translations[dialog.text])
         dialog.text = this.translations[dialog.text]
       return dialog
     })
