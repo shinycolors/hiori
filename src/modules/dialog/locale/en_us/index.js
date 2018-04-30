f78c1c2c5c218ec387a7ddb5f9bee2b8450db382
Jet Rico <jetriconew@gmail.com>
2018-04-30 10:58:34 +0800

    Include hiori-end scene

--- src/modules/dialog/locale/en_us/index.js
+++ src/modules/dialog/locale/en_us/index.js
@@ -1,6 +1,8 @@
 import names from './names.json'
 import produce from './produce.json'
+import hioriEnd from './hiori-end.json'
 
 export default []
   .concat(names.messages)
   .concat(produce.messages)
+  .concat(hioriEnd.messages)
